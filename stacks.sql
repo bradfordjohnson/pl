af4491ecd0d2375a456a9a667589c7153b13c9dd
Ford Johnson <111791965+bradfordjohnson@users.noreply.github.com>
2024-11-03 22:13:45 -0500

    Update stacks.sql

--- stacks.sql
+++ stacks.sql
@@ -2,9 +2,18 @@ WITH cte AS
   (SELECT id,
           REGEXP_REPLACE(LOWER(REPLACE(name, source_extension, '')), '\..*$', '') AS name,
           REPLACE(source_path, name, '') AS source_dir
-   FROM sidecar)
+   FROM sidecar),
+   hashes AS (
+	SELECT
+		sha256
+	FROM media
+	GROUP BY 1
+	HAVING(COUNT(DISTINCT id) > 1)
+)
 SELECT media.id,
+	   extension,
        source_dir,
+	   CASE WHEN COUNT(media.id) OVER(PARTITION BY cte.name) > 1 THEN TRUE ELSE FALSE END AS has_multiple_files_with_same_name,
        REPLACE(LOWER(source_name), source_extension, '') AS media_name,
        cte.name,
        cte.id AS sidecar_id
@@ -12,3 +21,5 @@ FROM media
 LEFT OUTER JOIN cte ON cte.source_dir = REPLACE(source_path, source_name, '')
 AND cte.name LIKE CONCAT(REPLACE(LOWER(source_name), source_extension, ''))
 WHERE cte.id IS NOT NULL
+AND media.sha256 NOT IN (SELECT sha256 FROM hashes)
+ORDER BY name
