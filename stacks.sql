5523d9eda31bff4c1c52a0e1146fad51f9b5bb1c
Ford Johnson <111791965+bradfordjohnson@users.noreply.github.com>
2024-11-03 22:33:53 -0500

    Update stacks.sql

--- stacks.sql
+++ stacks.sql
@@ -9,15 +9,13 @@ WITH cte AS
 	GROUP BY 1
 	HAVING(COUNT(DISTINCT id) > 1))
 SELECT media.id,
-	   extension,
+       extension,
        source_dir,
-	   CASE WHEN media.sha256 IN (SELECT sha256 FROM hashes) THEN TRUE ELSE FALSE END AS has_duplicate_hashes,
-	   CASE WHEN COUNT(media.id) OVER(PARTITION BY cte.name) > 1 THEN TRUE ELSE FALSE END AS has_multiple_files_with_similar_name,
        REPLACE(LOWER(source_name), source_extension, '') AS media_name,
-       cte.name,
+       CASE WHEN media.sha256 IN (SELECT sha256 FROM hashes) THEN TRUE ELSE FALSE END AS has_duplicate_hashes,
+       CASE WHEN COUNT(media.id) OVER(PARTITION BY cte.name) > 1 THEN TRUE ELSE FALSE END AS has_multiple_files_with_similar_name,
+       CASE WHEN cte.id IS NOT NULL THEN TRUE ELSE FALSE END AS has_sidecar_file,
        cte.id AS sidecar_id
 FROM media
 LEFT OUTER JOIN cte ON cte.source_dir = REPLACE(source_path, source_name, '')
 AND cte.name LIKE CONCAT(REPLACE(LOWER(source_name), source_extension, ''))
-WHERE cte.id IS NOT NULL
-ORDER BY name
