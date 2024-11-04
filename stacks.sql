2b170e34af825873c7e46f839ac35fd9744a9bfd
Ford Johnson <111791965+bradfordjohnson@users.noreply.github.com>
2024-11-03 21:43:20 -0500

    Update stacks.sql

--- stacks.sql
+++ stacks.sql
@@ -1,14 +1,14 @@
 WITH cte AS
   (SELECT id,
-          regexp_replace(lower(replace(name, source_extension, '')), '\..*$', '') AS name,
-          replace(source_path, name, '') AS source_dir
+          REGEXP_REPLACE(LOWER(REPLACE(name, source_extension, '')), '\..*$', '') AS name,
+          REPLACE(source_path, name, '') AS source_dir
    FROM sidecar)
 SELECT media.id,
        source_dir,
-       replace(lower(source_name), source_extension, '') AS media_name,
+       REPLACE(LOWER(source_name), source_extension, '') AS media_name,
        cte.name,
        cte.id AS sidecar_id
 FROM media
-LEFT OUTER JOIN cte ON cte.source_dir = replace(source_path, source_name, '')
-AND cte.name LIKE concat(replace(lower(source_name), source_extension, ''))
+LEFT OUTER JOIN cte ON cte.source_dir = REPLACE(source_path, source_name, '')
+AND cte.name LIKE CONCAT(REPLACE(LOWER(source_name), source_extension, ''))
 WHERE cte.id IS NOT NULL
